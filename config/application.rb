cc3e93217b984eb3ec56a43a58aa1096e29e955f
wacawa <mikkunmitsuaki@gmail.com>
2021-11-22 15:31:59 +0900

    finish japanization-and-time

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module PortfolioApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
