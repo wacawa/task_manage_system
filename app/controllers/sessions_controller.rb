239227aba65f2873cf74ded9e13ef7e8e6b6ea57
wacawa <mikkunmitsuaki@gmail.com>
2022-02-10 16:10:44 +0900

    edit-taskブランチからcreate_lineを引っ張ってきた。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -39,7 +39,33 @@ class SessionsController < ApplicationController
   end
 
   def create_line
-    debugger
+    if params[:code]
+      uri = URI("https://api.line.me/oauth2/v2.1/token")
+      parameters = {grant_type: "authorization_code", code: params[:code],  redirect_uri: "http://localhost:3000/auth/line",
+                    client_id: ENV["LINE_CLIENT_ID"], client_secret: ENV["LINE_CLIENT_SECRET"], code_verifier: SecureRandom.alphanumeric(100)}
+      res_body = get_resbody(uri, parameters)
+      if id_t = res_body["id_token"].presence
+        a_token = res_body["access_token"].presence
+        uri = URI('https://api.line.me/oauth2/v2.1/verify')
+        parameters = {id_token: id_t, client_id: ENV["LINE_CLIENT_ID"]}
+        res_body = get_resbody(uri, parameters)
+        res_body["a_token"] = a_token
+        res_body["provider"] = "line"
+        user = User.line_omniauth(res_body)
+        user.id ||= User.last.id + 1
+        password ||= SecureRandom.urlsafe_base64
+        user.password = password
+        if user.save
+          redirect_user(user, "lineログインに成功しました。")
+        else
+          flash.now[:_] = "もう一度お願いします。"
+          render "users/show"
+        end
+      end
+    elsif params[:error]
+      flash.now[:_] = "LINEログインに失敗しました。"
+      render "users/show"
+    end
   end
 
   def destroy
